56fda1f15c57528dfe6ddbd2038b7bd94a4e23b3
Alec Osborne <alec.weston.osborne@gmail.com>
2018-05-10 12:26:57 -0600

    alec

---
+++ Battleship/View Controllers/InitialViewController.swift
@@ -0,0 +1,26 @@
+//
+//  InitialViewController.swift
+//  Battleship
+//
+//  Created by Alec Osborne on 5/10/18.
+//  Copyright © 2018 UnInfamous Games. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    // MARK: - Properties
+    let centerLineAnchorView = UIView()
+    
+    
+    // MARK: - 	
+}
